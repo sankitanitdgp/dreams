7b9895969e93920015d430e7ca5ead5652aa928a
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2018-10-03 14:02:49 +0300

    Update user model

--- src/models/User.py
+++ src/models/User.py
@@ -5,10 +5,13 @@ class User:
 	password = ''
 	active = False
 
-	# set self.first_name, etc. here when implemented
-	def __init__(self):
-		print('User object created')
-	
+	def __init__(self, first_name, last_name, username, password, active=False):
+		self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+		self.active = active
+
 	# add type assertions for all placeholder params
 	def update_profile(self, str_p1, str_p2, str_p3, str_p4):
 		return None
